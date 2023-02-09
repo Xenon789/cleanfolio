26cc9fd9c6f21d78fd0eaf8ada00abe06697590e
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 23:33:04 -0800

    new push

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://xenon789.github.io/cleanfolio/',
+  title: 'XS.',
 }
 
 const about = {
